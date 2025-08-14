e85a8499b35adc33acd84859e0354d31ec8bceb8
Syvnox <Chavo.moser@icloud.com>
2025-08-14 22:31:21 +0200

    3deffect

--- cron/frontend/node-app/public/js/login.js
+++ cron/frontend/node-app/public/js/login.js
@@ -5,3 +5,19 @@ document.querySelector('.login-form').addEventListener('submit', function(e) {
     // TODO: Add AJAX call to backend for authentication
     alert(`Username: ${username}\nPassword: ${password}`);
 });
+
+// 3D tilt effect for login container
+const loginContainer = document.querySelector('.login-container');
+if (loginContainer) {
+    document.addEventListener('mousemove', (e) => {
+        const rect = loginContainer.getBoundingClientRect();
+        const x = e.clientX - rect.left - rect.width / 2;
+        const y = e.clientY - rect.top - rect.height / 2;
+        const rotateX = (y / rect.height) * 15;
+        const rotateY = -(x / rect.width) * 15;
+        loginContainer.style.transform = `perspective(800px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+    document.addEventListener('mouseleave', () => {
+        loginContainer.style.transform = 'perspective(800px) rotateX(0deg) rotateY(0deg)';
+    });
+}

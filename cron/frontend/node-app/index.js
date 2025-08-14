d0a24290f4c473abbe369fa495f0afffdd07707a
Syvnox <Chavo.moser@icloud.com>
2025-08-14 22:26:29 +0200

    login

--- cron/frontend/node-app/index.js
+++ cron/frontend/node-app/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redirect root to /login/
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.redirect('/login/');
+});
+
+// Serve static files (css, js, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve login page
+app.get('/login/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'login.html'));
 });
 
 app.listen(PORT, () => {
